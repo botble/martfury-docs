185a0dc8f34f43284ef5e23a3627d2f9f0ab732b
Sang Nguyen <sangnguyenplus@gmail.com>
2023-08-16 14:56:08 +0700

    Update docs

--- sidebar.ts
+++ sidebar.ts
@@ -18,25 +18,22 @@ export default [
     ],
   },
   {
-    text: 'Available plugins',
-    items: [
-      { text: 'Backup', link: '/martfury/plugin-backup' },
-    ],
-  },
-  {
     text: 'Usage',
     items: [
+      { text: 'Rename theme', link: '/martfury/theme-rename' },
       { text: 'Custom CSS/JS', link: '/martfury/usage-custom-css-js' },
       { text: 'Translation', link: '/martfury/usage-translation' },
-      { text: 'Email', link: '/martfury/usage-email' },
       { text: 'Multi-language', link: '/martfury/usage-multi-language' },
-      { text: 'Currencies', link: '/martfury/usage-currencies' },
+      { text: 'Location', link: '/martfury/usage-location' },
+      { text: 'Google Analytics', link: '/martfury/usage-analytics' },
       { text: 'Media - Setup Amazon S3', link: '/martfury/usage-media-s3' },
       { text: 'Media - Setup BunnyCDN', link: '/martfury/usage-media-bunnycdn' },
       { text: 'Media - Setup Wasabi', link: '/martfury/usage-media-wasabi' },
-      { text: 'Analytics', link: '/martfury/usage-analytics' },
-      { text: 'Rename theme', link: '/martfury/theme-rename' },
-      { text: 'Backup', link: '/martfury/usage-backup' },
+      { text: 'Setup email', link: '/martfury/usage-email' },
+      { text: 'Backup', link: '/martfury/plugin-backup' },
+
+      { text: 'Currencies', link: '/martfury/usage-currencies' },
+      { text: 'Setup social login', link: '/martfury/usage-social-login' },
       { text: 'Location', link: '/martfury/usage-location' },
       { text: 'Invoice template', link: '/martfury/invoice-template' },
     ],
