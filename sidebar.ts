2857bb273d405a848820e25b11299aaa5fec3e4e
Sang Nguyen <sangnguyenplus@gmail.com>
2023-11-23 17:43:07 +0700

    Update docs

--- sidebar.ts
+++ sidebar.ts
@@ -24,13 +24,11 @@ export default [
       { text: 'Custom CSS/JS', link: '/martfury/usage-custom-css-js' },
       { text: 'Translation', link: '/martfury/usage-translation' },
       { text: 'Multi-language', link: '/martfury/usage-multi-language' },
-      { text: 'Location', link: '/martfury/usage-location' },
       { text: 'Google Analytics', link: '/martfury/usage-analytics' },
       { text: 'Media - Setup Amazon S3', link: '/martfury/usage-media-s3' },
       { text: 'Media - Setup BunnyCDN', link: '/martfury/usage-media-bunnycdn' },
       { text: 'Media - Setup Wasabi', link: '/martfury/usage-media-wasabi' },
       { text: 'Setup email', link: '/martfury/usage-email' },
-      { text: 'Backup', link: '/martfury/plugin-backup' },
 
       { text: 'Currencies', link: '/martfury/usage-currencies' },
       { text: 'Setup social login', link: '/martfury/usage-social-login' },
